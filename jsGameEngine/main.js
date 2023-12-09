66b4f7ff4792ac950f30d0af60a7d3537931e25e
Al3x-K <ola.kail1901@gmail.com>
2023-12-09 13:10:53 +0000

    added a very basic player

--- jsGameEngine/main.js
+++ jsGameEngine/main.js
@@ -1,7 +1,17 @@
 const canvas = document.querySelector('canvas'); 
 const ctx = canvas.getContext('2d');  
 
-
-
 ctx.fillStyle = 'red';
-ctx.fillRect(0,0,canvas.width,canvas.height);
+ctx.fillRect(20,10,10,10);
+let y = 10;
+function gameLoop()
+{
+    window.requestAnimationFrame(gameLoop);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0,0,canvas.width,canvas.height);    
+    ctx.fillStyle = 'red';
+    ctx.fillRect(20,y,10,10);
+    y++;
+}
+
+gameLoop();
