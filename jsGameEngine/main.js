9104b256dc2a0d342eace36594867b00d02a57cd
Al3x-K <ola.kail1901@gmail.com>
2023-12-09 13:21:20 +0000

    implemented player class and added basic gravity

--- jsGameEngine/main.js
+++ jsGameEngine/main.js
@@ -1,17 +1,40 @@
 const canvas = document.querySelector('canvas'); 
 const ctx = canvas.getContext('2d');  
 
-ctx.fillStyle = 'red';
-ctx.fillRect(20,10,10,10);
+const gravity = 0.5;
+
+class Player
+{  
+    constructor()
+    {
+        this.position = {x: 0, y: 0};
+        this.velocity = {x: 0, y: 1};
+    }
+
+    draw()
+    {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y,10,10);
+    }
+
+    update()
+    {
+        this.draw();
+        this.position.y += this.velocity.y;
+        this.velocity.y += gravity;
+    }
+
+}
+
+const player = new Player();
+
 let y = 10;
 function gameLoop()
 {
     window.requestAnimationFrame(gameLoop);
     ctx.fillStyle = 'white';
     ctx.fillRect(0,0,canvas.width,canvas.height);    
-    ctx.fillStyle = 'red';
-    ctx.fillRect(20,y,10,10);
-    y++;
+    player.update();
 }
 
 gameLoop();
