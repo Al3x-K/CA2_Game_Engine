ef998fab5bef83140b5309391491de64e65c25ba
Al3x-K <ola.kail1901@gmail.com>
2023-12-10 00:50:15 +0000

    added gameObject class

---
+++ jsGameEngine/js/Engine/gamobject.js
@@ -0,0 +1,38 @@
+class GameObject
+{
+    constructor(x,y)
+    {
+        this.x =x;
+        this.y =y;
+        this.components = [];
+    }
+
+    addComponent(component)
+    {
+        this.components.push(component);
+        component.gameObject = this;
+    }   
+
+    update(deltaTime)
+    {
+        for (const component of this.components) 
+        {
+            component.update(deltaTime);
+        }
+    }
+
+    draw(ctx)
+    {
+        for (const component of this.components) 
+        {
+            component.draw(ctx);
+        }
+    }
+
+    getComponent(componentClass)
+    {
+        return this.components.find(component => component instanceof componentClass);
+    }
+}
+
+export default GameObject;
