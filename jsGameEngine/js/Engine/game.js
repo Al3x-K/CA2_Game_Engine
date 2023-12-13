019e281917ffb42cb77aa72f2484664460bfb0dc
Al3x-K <ola.kail1901@gmail.com>
2023-12-13 09:43:06 +0000

    added camera

--- jsGameEngine/js/Engine/game.js
+++ jsGameEngine/js/Engine/game.js
@@ -1,3 +1,5 @@
+import Camera from "./camera.js";
+
 class Game 
 {
     constructor(canvasId)
@@ -10,7 +12,7 @@ class Game
         this.deltaTime = 0;
         this.resizeCanvas();
         window.addEventListener('resize', () => this.resizeCanvas());
-        //this.camera = new Camera(null,this.canvas.width,this.canvas.height);
+        this.camera = new Camera(null,this.canvas.width,this.canvas.height);
     }
 
     resizeCanvas()
@@ -35,7 +37,7 @@ class Game
         this.lastFrameTime = currentTime; 
         this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height); // clear the canvas
         this.update();
-        //this.camera.update();
+        this.camera.update();
         this.draw();
         requestAnimationFrame((timestamp) => this.gameLoop(timestamp));
     }
